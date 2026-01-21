2a334492da2c161b40fcda3a30cec942dfd8eb48
frankdeveloper95 <e1315352557@live.uleam.edu.ec>
2026-01-21 07:43:34 -0500

    fix for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,7 @@ import fs from "fs";
 export default defineConfig({
 	server: {
 		// origin: 'http://localhost:8000'
-		https: {
-			cert: fs.readFileSync('/home/frank/Documents/tours_proyecto/Tours_Backend/pptours.com+1.pem'),
-			key: fs.readFileSync('/home/frank/Documents/tours_proyecto/Tours_Backend/pptours.com+1-key.pem')
-		},
+
 		host: '0.0.0.0'
 	},
 	plugins: [tailwindcss(), sveltekit()]
